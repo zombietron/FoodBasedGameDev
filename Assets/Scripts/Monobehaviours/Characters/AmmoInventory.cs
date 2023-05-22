6ad16468399258a92082ee5f34d1a877556f3f24
Johann Ungerer <johannu@azuro.co.za>
2023-05-22 12:46:49 -0600

    Fixed merge conflicts.

--- Assets/Scripts/Monobehaviours/Characters/AmmoInventory.cs
+++ Assets/Scripts/Monobehaviours/Characters/AmmoInventory.cs
@@ -4,66 +4,53 @@ using UnityEngine;
 
 public class AmmoInventory : MonoBehaviour
 {
-    //ammo inventory handles anything that goes into adding or subtracting ammo
-    private readonly Dictionary<FoodType, int> ammoDictionary = new();
+	//ammo inventory handles anything that goes into adding or subtracting ammo
+	private readonly Dictionary<FoodType, int> ammoDictionary = new();
 
-    [SerializeField]
-    private AmmoContainer ammoContainer;
+	[SerializeField]
+	private AmmoContainer ammoContainer;
 
-    public int this[FoodType key]
-    {
-        get { return ammoDictionary.ContainsKey(key) ? ammoDictionary[key] : 0; }
-    }
+	public int this[FoodType key]
+	{
+		get { return ammoDictionary.ContainsKey(key) ? ammoDictionary[key] : 0; }
+	}
 
-    public bool AddAmmo(FoodType key, int val)
-    {
-        var maxAmmo = ammoContainer.ammoList.FirstOrDefault(a => a.GetFoodType() == key);
-        int maxAmmoAmt = maxAmmo == null ? 0 : maxAmmo.MaxAmmoAmt;
+	//	Returns foodtypes in the dictionary. If all is true, will return regardless of ammo level, else only where there's ammo
+	public List<FoodType> GetAvailableAmmo(bool all = false)
+	{
+		return all ? ammoDictionary.Keys.ToList() : ammoDictionary.Where(ft => ft.Value > 0).ToList().ConvertAll(ft => ft.Key);
+	}
 
-        if (ammoDictionary.ContainsKey(key))
-        {
-            ammoDictionary[key] = System.Math.Min(ammoDictionary[key] + val, maxAmmoAmt);
-            return true;
-        }
-        else
-            return ammoDictionary.TryAdd(key, System.Math.Min(val, maxAmmoAmt));
-    }
+	public bool AddAmmo(FoodType key, int val)
+	{
+		var maxAmmo = ammoContainer.ammoList.FirstOrDefault(a => a.GetFoodType() == key);
+		int maxAmmoAmt = maxAmmo == null ? 0 : maxAmmo.MaxAmmoAmt;
 
+		if (ammoDictionary.ContainsKey(key))
+		{
+			ammoDictionary[key] = System.Math.Min(ammoDictionary[key] + val, maxAmmoAmt);
+			return true;
+		}
+		else
+			return ammoDictionary.TryAdd(key, System.Math.Min(val, maxAmmoAmt));
+	}
 
-
-    // public FoodType UpdateAmmoInventory(Ammo ammoType, int ammoAmt)
-    // {
-    //     Debug.Log("Update Ammo Inventory entered");
-    //     //compare current ammo to max ammo allowed for type
-    //     int maxAmmo = ammoAmt;
-    //     int currentAmmo;
-
-    //     bool exists = ammoDictionary.TryGetValue(ammoType.GetFoodType().ToString(), out currentAmmo);
-    //     Debug.Log(currentAmmo);
-    //     if (exists && currentAmmo < maxAmmo)
-    //     {
-    //         ammoDictionary[ammoType.GetFoodType().ToString()] = maxAmmo;
-    //         Debug.Log($"{ammoType.GetFoodType()} has been reloaded and has {ammoDictionary[ammoType.GetFoodType().ToString()]}");
-    //     }
-
-    //     return ammoType.GetFoodType();
-    // }
-
-
-    public void ShootAndRemoveAmmoInventory(Ammo ammoType)
-    {
-        var currentAmmoAmount = ammoDictionary[ammoType.GetFoodType()];
-        if (currentAmmoAmount <= 0)
-        {
-            //Add no remaining ammo sound
-            return;
-        }
-        //add animation trigger here or player?
-        //add shooting sound here?
-        ammoDictionary[ammoType.GetFoodType()] = currentAmmoAmount - 1;
-    }
-    
-    //tryAddAmmo (ammo type, amount)
-    //check dictionary, if valid, add, else do nothing
-
+	public void ShootAndRemoveAmmoInventory(Ammo ammoType)
+	{
+		//	Make sure the ammo Type is loaded.
+		if (ammoDictionary.ContainsKey(ammoType.GetFoodType()))
+		{
+			if (ammoDictionary[ammoType.GetFoodType()]-- <= 0)
+			{
+				ammoDictionary[ammoType.GetFoodType()] = 0;
+				//Add no remaining ammo sound
+				return;
+			}
+			else
+			{
+				//add animation trigger here or player?
+				//add shooting sound here? - I would recommend not - separation of concerns
+			}
+		}
+	}
 }
--- Assets/Scripts/Monobehaviours/Characters/PlayerController.cs
+++ Assets/Scripts/Monobehaviours/Characters/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,27 +13,27 @@ namespace Monobehaviours.Characters
 		[SerializeField] private AmmoInventory characterAmmoInventory;
 		//[SerializeField] private Dictionary<string, int> characterAmmoDict;
 		[SerializeField] private StallBehavior stallBehavior; //change to stall type; add identifier to each stall type 
-        [SerializeField] private float rotateSpeed;
+		[SerializeField] private float rotateSpeed;
 		public bool stopPlayer = false;
 		private int currentAmmoCount;
-        // private List<string> allFoodTypes = new List<string>();// not required in player controller anymore?
+		private List<FoodType> allFoodTypes = new();// not required in player controller anymore?
 		private Vector2 move;
 		private Vector3 moveForce;
-        private string activeFoodTypeToThrow;
-        private float rotateDirection;
+		private FoodType activeFoodTypeToThrow;
+		private float rotateDirection;
 		private bool isInteractable;
 
-        void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-            characterAmmoInventory = GetComponent<AmmoInventory>();
-			//	TODO: This can be done differently
-            // characterAmmoDict = characterAmmoInventory.GetAmmoDict();
-            // foreach (var key in characterAmmoDict.Keys)
-            // {
-            //     allFoodTypes.Add(key); 
-            // }
-        }
+		void Start()
+		{
+			rb = GetComponent<Rigidbody>();
+			characterAmmoInventory = GetComponent<AmmoInventory>();
+			//	Get available food types from the Enum and populate the list.
+			var foodTypes = System.Enum.GetValues(typeof(FoodType));
+			foreach (var ft in foodTypes)
+			{
+				allFoodTypes.Add((FoodType)ft);
+			}
+		}
 
 		void OnMove(InputValue value)
 		{
@@ -40,23 +41,25 @@ namespace Monobehaviours.Characters
 			moveForce = new Vector3(-move.x, 0.0f, -move.y);
 		}
 
-        public void OnInteract(InputValue value)
-        {
+		public void OnInteract(InputValue value)
+		{
 
-            if (!isInteractable) return;
-            stallBehavior.StartAmmoPickupCycle(value.isPressed);
-            // trigger pick up animation?
-        }
+			if (!isInteractable)
+				return;
+			stallBehavior.StartAmmoPickupCycle(value.isPressed);
+			// trigger pick up animation?
+		}
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (!other.gameObject.CompareTag("foodStall")) return;
-            stallBehavior = other.gameObject.GetComponent<StallBehavior>();
-            stallBehavior.DisplayUseInstructions(true);
-            stallBehavior.Inventory = characterAmmoInventory;
-            activeFoodTypeToThrow = stallBehavior.GetTableAmmoType().GetFoodType().ToString();
-            isInteractable = true;
-        }
+		private void OnTriggerEnter(Collider other)
+		{
+			if (!other.gameObject.CompareTag("foodStall"))
+				return;
+			stallBehavior = other.gameObject.GetComponent<StallBehavior>();
+			stallBehavior.DisplayUseInstructions(true);
+			stallBehavior.Inventory = characterAmmoInventory;
+			activeFoodTypeToThrow = stallBehavior.GetTableAmmoType().GetFoodType();
+			isInteractable = true;
+		}
 
 		private void OnTriggerExit(Collider other)
 		{
@@ -66,48 +69,51 @@ namespace Monobehaviours.Characters
 			isInteractable = false;
 		}
 
-        void OnToggle(InputValue value)
-        {
-            //TODO: Add Food projectile toggle code
-            foreach (var foodType in allFoodTypes)
-            {
-                if (foodType == activeFoodTypeToThrow) continue;
-                activeFoodTypeToThrow = foodType;
-                return;
-            }
+		void OnToggle(InputValue value)
+		{
+			//	TODO: I would recommend fetching the available foodTypes at runtime, since you might not have ammo of that type
+			var alernativeFoodList = characterAmmoInventory.GetAvailableAmmo();
+			//TODO: Add Food projectile toggle code
+			foreach (var foodType in allFoodTypes)
+			{
+				if (foodType == activeFoodTypeToThrow)
+					continue;
+				activeFoodTypeToThrow = foodType;
+				return;
+			}
 
-            Debug.Log("Active Food Type: " + activeFoodTypeToThrow); // will be easier to test once we have other tables and add other ammos
-        }
+			Debug.Log("Active Food Type: " + activeFoodTypeToThrow); // will be easier to test once we have other tables and add other ammos
+		}
 
-        void OnRotate(InputValue value)
-        {
-            Debug.Log("Triggering Rotate");
-            rotateDirection = value.Get<float>();
-        }
-        void OnThrow()
-        {
-            //add throw animation here
-            isInteractable = false;
-        }
+		void OnRotate(InputValue value)
+		{
+			Debug.Log("Triggering Rotate");
+			rotateDirection = value.Get<float>();
+		}
+		void OnThrow()
+		{
+			//add throw animation here
+			isInteractable = false;
+		}
 
 		void OnLook(InputValue value)
 		{
 			//TODO: Aim to shoot maybe?
 		}
 
-        private void FixedUpdate()
-        {
-            rb.velocity = moveForce * moveSpeed; //change to updating transform instead
-            transform.Rotate(Vector3.up * (Time.deltaTime * rotateSpeed * rotateDirection));
-            #region ammo transform follows player transform
-            //if (isInteractable)
-            //{
-            //    foreach (var pizza in pizzasToThrow)
-            //    {
-            //        pizza.transform.position = transform.position;
-            //    }   
-            //}
-            #endregion
+		private void FixedUpdate()
+		{
+			rb.velocity = moveForce * moveSpeed; //change to updating transform instead
+			transform.Rotate(Vector3.up * (Time.deltaTime * rotateSpeed * rotateDirection));
+			#region ammo transform follows player transform
+			//if (isInteractable)
+			//{
+			//    foreach (var pizza in pizzasToThrow)
+			//    {
+			//        pizza.transform.position = transform.position;
+			//    }   
+			//}
+			#endregion
 
 			if (!stopPlayer)
 				return;
